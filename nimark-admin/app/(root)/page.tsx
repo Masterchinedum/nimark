747e56ca6e389141b792ca9a2fe6f7d409f71f34
CHINEDU KINGSLEY OKEREKE <Chinedu02k@gmail.com>
2024-06-30 20:51:15 +0000

    chore: Add button component to home page

--- nimark-admin/app/(root)/page.tsx
+++ nimark-admin/app/(root)/page.tsx
@@ -1,13 +1,16 @@
+import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
   return (
-    <div>
+    <div className="p-10">
       <h1>Welcome to Nimark Admin</h1>
       <p>
         Nimark Admin is a dashboard for managing your Nimark account.
       </p>
-
+      <Button className="mt-10" variant="default">
+        Get Started
+      </Button>
     </div>
   );
 }
