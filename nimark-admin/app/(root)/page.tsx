d9bda2cbaf64f7cbeb492bfd386f2b4ff3f48bfa
CHINEDU KINGSLEY OKEREKE <Chinedu02k@gmail.com>
2024-07-03 18:44:47 +0000

    chore: Update SetupPage component to use Modal for account setup

--- nimark-admin/app/(root)/page.tsx
+++ nimark-admin/app/(root)/page.tsx
@@ -1,23 +1,15 @@
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
+"use client";
 
+import { Modal } from "@/components/ui/modal";
 
-export default function SetupPage() {
+const SetupPage = () => {
   return (
-    <div className="p-10">
-      <UserButton  afterSignOutUrl="/" />
-      <h1>Welcome to Nimark Admin</h1>
-      <p>
-        Nimark Admin is a dashboard for managing your Nimark account.
-      </p>
-      <div className="flex-1">
-        <Button className="m-10" variant="default">
-          Get Started
-        </Button>
-        <Button>
-         Check News
-        </Button>
-      </div>
+    <div className="p-4">
+      <Modal title="Setup" description="Setup your account" isOpen onClose={() => {}}>
+        Children
+      </Modal>
     </div>
   );
-}
+};
+
+export default SetupPage;
