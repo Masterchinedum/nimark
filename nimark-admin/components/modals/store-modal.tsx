5886759ca8085cb6dee6e38d982ecaa9fec894ec
CHINEDU KINGSLEY OKEREKE <Chinedu02k@gmail.com>
2024-07-08 14:56:44 +0000

    chore: Update StoreModal component to use dynamic isOpen and onClose props

--- nimark-admin/components/modals/store-modal.tsx
+++ nimark-admin/components/modals/store-modal.tsx
@@ -1,11 +1,25 @@
 "use client"
 
+import * as z from 'zod';
 import { useStoreModal } from '@/hooks/use-store-modal';
 import { Modal } from "@/components/ui/modal";
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 
+
+const formSchema = z.object({
+    name: z.string().min(1)
+});
 
 export const StoreModal = () => {
     const StoreModal = useStoreModal();
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: ""
+        }
+    })
 
     return (
         <Modal
