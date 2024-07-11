0d780b18e2171a26080c2a062d8071b583bee6bb
CHINEDU KINGSLEY OKEREKE <Chinedu02k@gmail.com>
2024-07-11 01:26:21 +0000

    chore: Add DashboardPage component to store dashboard routes

---
+++ nimark-admin/app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -0,0 +1,13 @@
+import { date } from "zod";
+
+
+const DashboardPage = () => {
+  return (
+    <div>
+        <h1>Let this be the Dashboard</h1>
+        <p>Here we will have the dashboard creates</p>
+    </div>
+  )
+}
+
+export default DashboardPage;
