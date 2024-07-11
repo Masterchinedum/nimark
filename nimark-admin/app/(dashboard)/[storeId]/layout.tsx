b1464ee7d2b4a93f4eede9b7208a658d1c1bb718
CHINEDU KINGSLEY OKEREKE <Chinedu02k@gmail.com>
2024-07-11 01:13:39 +0000

    chore: Add layout component for store dashboard

---
+++ nimark-admin/app/(dashboard)/[storeId]/layout.tsx
@@ -0,0 +1,11 @@
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+
+export default function Layout() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+    </div>
+  );
+}
