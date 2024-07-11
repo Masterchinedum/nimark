8f4e0fefafcecd0b23988d6b402c251517d067ca
CHINEDU KINGSLEY OKEREKE <Chinedu02k@gmail.com>
2024-07-11 22:11:16 +0000

    Refactor navbar component to include user button and improve layout

--- nimark-admin/components/navbar.tsx
+++ nimark-admin/components/navbar.tsx
@@ -1,22 +1,37 @@
-import { UserButton } from "@clerk/nextjs";
+import React from 'react'
+import { UserButton } from '@clerk/nextjs';
+import { auth } from '@clerk/nextjs/server';
+import { MainNav } from '@/components/main-nav';
+import StoreSwitcher from '@/components/store-switcher';
+import { redirect } from 'next/navigation'
+import prismadb from '@/lib/prismadb';
+import { ThemeToggle } from '@/components/theme-toggle';
 
+const Navbar = async () => {
+  const { userId } = auth();
 
-const Navbar = () => {
-    return (
-        <div className="border-b">
-            <div className="flex h-15 items-center px-4">
-                <div>
-                    This will be a store switcher
-                </div>
-                <div>
-                    This will be the routes
-                </div>
-                <div className="nl-auto flex items-center space-x-4">
-                    <UserButton afterSignOutUrl="/"/>
-                </div>
+  if(!userId) {
+    redirect("/sign-in")
+  }
+
+  const stores = await prismadb?.store.findMany({
+    where: {
+      userId,
+    }
+  })
+
+  return (
+    <div className='border-b'>
+        <div className='flex items-center h-16 px-4'>
+            <StoreSwitcher items={stores} />
+            <MainNav className='mx-6' />
+            <div className='flex items-center ml-auto space-x-4'>
+              <ThemeToggle />
+                <UserButton afterSignOutUrl='/'/>
             </div>
         </div>
-    );
-};
+    </div>
+  )
+}
 
-export default Navbar;
+export default Navbar
