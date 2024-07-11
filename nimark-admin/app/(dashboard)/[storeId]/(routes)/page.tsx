5ac39c18837c65988e2b0a1cc3c7920f77c09b66
CHINEDU KINGSLEY OKEREKE <Chinedu02k@gmail.com>
2024-07-11 02:05:14 +0000

    Refactor store dashboard layout to use h1 heading for navbar

--- nimark-admin/app/(dashboard)/[storeId]/(routes)/page.tsx
+++ nimark-admin/app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,10 +1,8 @@
-import { date } from "zod";
-
 
 const DashboardPage = () => {
   return (
     <div>
-        <h1>Let this be the Dashboard</h1>
+        <h1 className="text-xl font-bold mb-4 text-gray-900">Let this be the Dashboard</h1>
         <p>Here we will have the dashboard creates</p>
     </div>
   )
