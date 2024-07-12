498863cd36a964b7393aa85418af5f59811b1d38
CHINEDU KINGSLEY OKEREKE <Chinedu02k@gmail.com>
2024-07-12 05:33:35 +0000

    Refactor navbar component to include user button and improve layout

--- nimark-admin/components/navbar.tsx
+++ nimark-admin/components/navbar.tsx
@@ -20,6 +20,7 @@ const Navbar = async () => {
     }
   })
 
+
   return (
     <div className='border-b'>
         <div className='flex items-center h-16 px-4'>
@@ -27,7 +28,7 @@ const Navbar = async () => {
             <MainNav className='mx-6' />
             <div className='flex items-center ml-auto space-x-4'>
               <ThemeToggle />
-                <UserButton afterSignOutUrl='/'/>
+                <UserButton />
             </div>
         </div>
     </div>
