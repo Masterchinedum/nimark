1b0cb974431fc7c7f60176347e120ef91e4380f0
CHINEDU KINGSLEY OKEREKE <Chinedu02k@gmail.com>
2024-07-13 16:03:23 +0000

    chore: Refactor AlertModal component for improved code organization and readability

--- nimark-admin/components/modals/alert-modal.tsx
+++ nimark-admin/components/modals/alert-modal.tsx
@@ -35,10 +35,16 @@ export const AlertModal: React.FC<AlertModalProps> = ({
             isOpen={isOpen}
             onClose={onClose}>
                 <div className="flex items-center justify-end w-full pt-6 space-x-2">
-                    <Button disabled={loading} variant="outline" onClick={onClose}>
+                    <Button 
+                        disabled={loading} 
+                        variant="outline" 
+                        onClick={onClose}>
                         Cancel
                     </Button>
-                    <Button disabled={loading} variant="destructive" onClick={onConfirm}>
+                    <Button 
+                        disabled={loading} 
+                        variant="destructive" 
+                        onClick={onConfirm}>
                         Continue
                     </Button>
                 </div>
