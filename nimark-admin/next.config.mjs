4fa0d283619651bd6e1f47eccdf02d85b52df59a
CHINEDU KINGSLEY OKEREKE <Chinedu02k@gmail.com>
2024-07-13 20:05:55 +0000

    chore: Remove unused code and update next.config.mjs

--- nimark-admin/next.config.mjs
+++ nimark-admin/next.config.mjs
@@ -1,17 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
     images: {
         domains: ['res.cloudinary.com'],
-    },
-    async headers() {
-        return [
-        {
-            source: '/(.*)',
-            headers: securityHeaders,
-        },
-        ];
-    },
+    }
 };
 
 export default nextConfig;
