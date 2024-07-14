fae19ec4716d58d4b9a20d543fd24de6d58f82a1
CHINEDU KINGSLEY OKEREKE <Chinedu02k@gmail.com>
2024-07-14 23:34:38 +0000

    chore: Update createdAt format in BillboardsPage component

--- nimark-admin/app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ nimark-admin/app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -21,7 +21,7 @@ const BillboardsPage = async ({
     const formattedBillboards: BillboardColumn[] = billboards.map(item => ({
         id: item.id,
         label: item.label,
-        createdAt: format(item.createdAt, "MMMM do, yyyy h:mm a"),
+        createdAt: format(item.createdAt, "MMMM do, yyyy / h:mm a"),
     }));
 
     return (
