fcc6b1e2545df81f79ca8d6e5bf574037c8134cc
CHINEDU KINGSLEY OKEREKE <Chinedu02k@gmail.com>
2024-07-19 11:54:48 +0000

    Refactor StoreSwitcher component to include a popover for store selection

--- nimark-admin/components/store-switcher.tsx
+++ nimark-admin/components/store-switcher.tsx
@@ -1,9 +1,13 @@
 "use client"
 
-import { PopoverTrigger } from "@/components/ui/popover";
+import { Popover, PopoverTrigger } from "@/components/ui/popover";
 import { useStoreModal } from "@/hooks/use-store-modal";
 import { Store } from "@prisma/client";
 import { useParams, useRouter } from "next/navigation";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Store as StoreIcon} from "lucide-react";
+import { cn } from "@/lib/utils";
 
 type PopoverTriggerProps = React.ComponentPropsWithoutRef<typeof PopoverTrigger>
 
@@ -22,11 +26,33 @@ export default function StoreSwitcher({
     const formattedItems = items.map((item) => ({
         label: item.name,
         value: item.id,
-    }))
+    }));
+
+    const [open, setOpen] = useState(false);
+
+    const currentStore = formattedItems.find((item) => item.value === params.storeId);
+
+    const onStoreSelect = (store: {value: string, label: string }) => {
+        setOpen(false);
+        router.push(`/${store.value}`);
+    }
 
     return (
-        <div>
-            Store Switcher
-        </div>
+        <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger asChild>
+                <Button
+                    variant="outline"
+                    size= "sm"
+                    role="combobox"
+                    aria-expanded={open}
+                    aria-label="Select a store"
+                    className={cn("w-[200px] justify-between", className)}
+                >
+                    <StoreIcon 
+                        className="mr-2 h-4 w-4"
+                        />
+                </Button>
+            </PopoverTrigger>
+        </Popover>
     )
 };
