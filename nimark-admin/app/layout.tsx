e759ceb867f096e7052557b98caf103f6a863783
CHINEDU KINGSLEY OKEREKE <Chinedu02k@gmail.com>
2024-07-19 10:15:17 +0000

    chore: Add ToasterProvider to RootLayout for displaying toast notifications

--- nimark-admin/app/layout.tsx
+++ nimark-admin/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import { ModalProvider } from '@/providers/modal-provider';
 
 import { ClerkProvider } from '@clerk/nextjs' ;
+import { ToasterProvider } from "@/providers/toast-provider";
 
 
 
@@ -23,6 +24,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
+          <ToasterProvider />
           <ModalProvider />
           {children}
         </body>
