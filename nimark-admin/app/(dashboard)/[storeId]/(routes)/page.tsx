5294a747efc189016d720e7a93c03510479f0d46
CHINEDU KINGSLEY OKEREKE <Chinedu02k@gmail.com>
2024-07-19 11:04:45 +0000

    Refactor DashboardPage component for improved store retrieval and code organization

--- nimark-admin/app/(dashboard)/[storeId]/(routes)/page.tsx
+++ nimark-admin/app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,9 +1,22 @@
+import prismadb from "@/lib/prismadb";
+import React from "react";
 
-const DashboardPage = () => {
+interface DashboardPageProps {
+  params: { storeId: string }
+};
+const DashboardPage: React.FC <DashboardPageProps>  = async ({ 
+  params 
+}) => {
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId
+    }
+  });
   return (
     <div>
         <h1 className="text-xl font-bold mb-4 text-gray-900">Let this be the Dashboard</h1>
         <p>Here we will have the dashboard creates</p>
+        <p> Active store: <b> {store?.name} </b> </p>
     </div>
   )
 }
