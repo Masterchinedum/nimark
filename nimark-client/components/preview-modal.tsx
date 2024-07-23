37f8e00267a9771dc19da39dcb3f1f86860b595f
CHINEDU KINGSLEY OKEREKE <Chinedu02k@gmail.com>
2024-07-23 18:25:43 +0000

    refactor: Remove unused code in PreviewModal component

--- nimark-client/components/preview-modal.tsx
+++ nimark-client/components/preview-modal.tsx
@@ -1,17 +1,7 @@
 "use client";
 
-import usePreviewModal from "@/hooks/use-preview-modal";
-import Modal from '@/components/ui/modal';
-import Gallery from "@/components/gallery";
-import Info from "@/components/info";
 
 const PreviewModal = () => {
-    const previewModal = usePreviewModal();
-    const product = usePreviewModal((state) => state.data);
-
-    if(!product) {
-        return null;
-    }
 
     return (
                 <div className="sm:col-span-4 lg:col-span-5">
