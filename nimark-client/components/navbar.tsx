ffb8ccfa0c3764255333e63408e2b79ec9f48353
CHINEDU KINGSLEY OKEREKE <Chinedu02k@gmail.com>
2024-07-23 15:10:23 +0000

    refactor: Update import statement for MainNav component in navbar.tsx

--- nimark-client/components/navbar.tsx
+++ nimark-client/components/navbar.tsx
@@ -1,6 +1,6 @@
 import Container from "@/components/ui/container";
 import Link from "next/link";
-import { MainNav } from "@/components/main-nav";
+import MainNav from "@/components/main-nav";
 import getCategories from "@/actions/get-categories";
 import NavbarActions from "./navbar-actions";
 
