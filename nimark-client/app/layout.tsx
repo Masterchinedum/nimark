5ac4f6b1efb7c196d78f1a8858ae309e59f976f3
CHINEDU KINGSLEY OKEREKE <Chinedu02k@gmail.com>
2024-07-23 15:16:46 +0000

    refactor: Update import statements for components in layout.tsx

--- nimark-client/app/layout.tsx
+++ nimark-client/app/layout.tsx
@@ -1,9 +1,10 @@
-import { Footer, Navbar } from '@/components'
-import './globals.css'
-import type { Metadata } from 'next'
-import { Urbanist } from 'next/font/google'
-import ModalProvider from '@/providers/modal-provider'
-import ToastProvider from '@/providers/toast-provider'
+import Footer from '@/components/footer';
+import Navbar from '@/components/navbar';
+import './globals.css';
+import type { Metadata } from 'next';
+import { Urbanist } from 'next/font/google';
+import ModalProvider from '@/providers/modal-provider';
+import ToastProvider from '@/providers/toast-provider';
 
 const urban = Urbanist({ subsets: ['latin'] })
 
