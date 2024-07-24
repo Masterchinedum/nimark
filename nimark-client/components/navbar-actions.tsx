bca2f5ad2f65f75c731d1baa1d8b8a42beb49d4b
CHINEDU KINGSLEY OKEREKE <Chinedu02k@gmail.com>
2024-07-24 14:44:45 +0000

    feat: Improve styling of ShoppingBag icon in NavbarActions component

--- nimark-client/components/navbar-actions.tsx
+++ nimark-client/components/navbar-actions.tsx
@@ -7,7 +7,7 @@ const NavbarActions = () => {
 
     return (
         <div className="flex items-center ml-auto gap-x-4">
-            <Button >
+            <Button className='flex items-center px-4 py-2 bg-black rounded-full'>
                 <ShoppingBag 
                     size={20}
                     color='white'
