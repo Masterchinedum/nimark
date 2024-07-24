c01b7eb2f413b6d2b9e47252d1f9887d1fa0f39b
CHINEDU KINGSLEY OKEREKE <Chinedu02k@gmail.com>
2024-07-24 14:43:11 +0000

    feat: Add shopping bag icon to NavbarActions component

--- nimark-client/components/navbar-actions.tsx
+++ nimark-client/components/navbar-actions.tsx
@@ -1,13 +1,14 @@
 "use client"
 
 import Button from '@/components/ui/button';
+import { ShoppingBag } from 'lucide-react';
 
 const NavbarActions = () => {
 
     return (
         <div className="flex items-center ml-auto gap-x-4">
-            <Button>
-                Login
+            <Button >
+                <ShoppingBag />
             </Button>
             This is an action
         </div>
