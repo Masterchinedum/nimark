f3d521c2cac4d3762b575ee0449608c2a089569d
CHINEDU KINGSLEY OKEREKE <Chinedu02k@gmail.com>
2024-07-24 14:41:52 +0000

    feat: Add login button to NavbarActions component and update npm dependency for lucide-react to version 0.414.0

--- nimark-client/components/navbar-actions.tsx
+++ nimark-client/components/navbar-actions.tsx
@@ -1,9 +1,14 @@
 "use client"
 
+import Button from '@/components/ui/button';
+
 const NavbarActions = () => {
 
     return (
         <div className="flex items-center ml-auto gap-x-4">
+            <Button>
+                Login
+            </Button>
             This is an action
         </div>
     )
