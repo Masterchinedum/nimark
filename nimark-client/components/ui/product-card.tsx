36b9fbbd00a41b77b51a2573cd64f8d116408b9f
CHINEDU KINGSLEY OKEREKE <Chinedu02k@gmail.com>
2024-07-25 13:36:54 +0000

    refactor: Remove unused code and comments in ProductCard component

--- nimark-client/components/ui/product-card.tsx
+++ nimark-client/components/ui/product-card.tsx
@@ -2,22 +2,22 @@
 
 import { Product } from "@/types";
 import Image from "next/image";
-import IconButton from "@/components/ui/icon-button";
+// import IconButton from "@/components/ui/icon-button";
 import { Expand, ShoppingCart } from "lucide-react";
-import Currency from "@/components/ui/currency";
+// import Currency from "@/components/ui/currency";
 import { useRouter } from "next/navigation";
-import PreviewModal from './../preview-modal';
-import usePreviewModal from "@/hooks/use-preview-modal";
+// import PreviewModal from './../preview-modal';
+// import usePreviewModal from "@/hooks/use-preview-modal";
 import { MouseEventHandler } from 'react';
-import useCart from "@/hooks/use-cart";
+// import useCart from "@/hooks/use-cart";
 
 interface ProductCard {
     data: Product;
 }
 
 const ProductCard: React.FC<ProductCard> = ({ data }) => {
-    const cart = useCart();
-    const previewModal = usePreviewModal();
+    // const cart = useCart();
+    // const previewModal = usePreviewModal();
     const router = useRouter();
     const handleClick = () => {
         router.push(`/product/${data?.id}`)
@@ -25,12 +25,12 @@ const ProductCard: React.FC<ProductCard> = ({ data }) => {
 
     const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
         event.stopPropagation();
-        previewModal.onOpen(data);
+        // previewModal.onOpen(data);
     }
 
     const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
         event.stopPropagation();
-        cart.addItem(data);
+        // cart.addItem(data);
     }
 
     return ( 
@@ -43,14 +43,14 @@ const ProductCard: React.FC<ProductCard> = ({ data }) => {
                     alt="Images"
                     className="object-cover rounded-md aspect-square" />
                 <div className="absolute w-full px-6 transition opacity-0 group-hover:opacity-100 bottom-5">
-                    <div className="flex justify-center gap-x-6">
+                    {/* <div className="flex justify-center gap-x-6">
                         <IconButton
                             onClick={onPreview}
                             icon={<Expand size={20} className="text-gray-600" />}/>
                         <IconButton
                             onClick={onAddToCart}
                             icon={<ShoppingCart size={20} className="text-gray-600" />}/>
-                    </div>
+                    </div> */}
                 </div>
             </div>
             {/* Description */}
@@ -63,9 +63,9 @@ const ProductCard: React.FC<ProductCard> = ({ data }) => {
                 </p>
             </div>
             {/* Price */}
-            <div className="flex items-center justify-between">
+            {/* <div className="flex items-center justify-between">
                 <Currency value={data?.price} />
-            </div>
+            </div> */}
         </div>
     );
 }
