752c05dda8dda79f51713cda50c161134e62e57a
CHINEDU KINGSLEY OKEREKE <Chinedu02k@gmail.com>
2024-07-25 14:58:55 +0000

    feat: Update ProductList component to directly display product names
    The ProductList component has been modified to directly display the names of the products instead of using the ProductCard component. This change simplifies the code and improves performance. Unused code in the HomePage component has also been removed.

--- nimark-client/components/product-list.tsx
+++ nimark-client/components/product-list.tsx
@@ -15,12 +15,12 @@ const ProductList: React.FC<ProductListProps> = ({ title, items }) => {
             <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
                 {items.map(item => (
                     <div key={item.id}>
-                        {item.name}
-                        {/* <ProductCard key={item.id} data={item} /> */}
+                        <ProductCard key={item.id} data={item} />
                     </div>
                 ))}
             </div>
         </div>
+        
      );
 }
  
