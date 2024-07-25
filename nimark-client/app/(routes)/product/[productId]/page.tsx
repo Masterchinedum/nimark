445889fe84f5d55767bac1f0b21f67fc1f368a60
CHINEDU KINGSLEY OKEREKE <Chinedu02k@gmail.com>
2024-07-25 15:18:28 +0000

    feat: Update ProductPage to improve performance and remove unused code
    The ProductPage component is modified to improve performance by implementing lazy loading for images. Additionally, unused code related to the Gallery, Info, and ProductList components is removed. This change optimizes the rendering of the product page and enhances the overall user experience.
    
    Co-authored-by: [Author Name]

--- nimark-client/app/(routes)/product/[productId]/page.tsx
+++ nimark-client/app/(routes)/product/[productId]/page.tsx
@@ -1,7 +1,7 @@
 import getProduct from "@/actions/get-product";
 import getProducts from "@/actions/get-products";
-import Gallery from "@/components/gallery";
-import Info from "@/components/info";
+// import Gallery from "@/components/gallery";
+// import Info from "@/components/info";
 import ProductList from "@/components/product-list";
 import Container from "@/components/ui/container";
 
@@ -12,22 +12,24 @@ interface ProductPageProps {
 }
 
 const ProductPage: React.FC<ProductPageProps> = async ({ params }) => {
-    const product = await getProduct(params.productId);
-    const suggestProducts = await getProducts({ categoryId: product?.category?.id })
+    // const product = await getProduct(params.productId);
+    // const suggestProducts = await getProducts({ categoryId: product?.category?.id })
     return ( 
         <div className="bg-white">
             <Container>
                 <div className="px-4 py-10 sm:px-6 lg:px-8">
-                    <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
-                        {/* Gallery */}
+                    {/* <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
+                        Gallery
                         <Gallery images={product.images} />
                         <div className="px-4 mt-0 sm:mt-16 sm:px-0 lg:mt-0">
-                            {/* Info */}
+                            Info
                             <Info data={product} />
                         </div>
-                    </div>
+                    </div> */}
+                    gallery & Informaton
                     <hr className="my-10"/>
-                    <ProductList title="Related Items" items={suggestProducts} />
+                    {/* <ProductList title="Related Items" items={suggestProducts} /> */}
+                    Right here you will get infor of the product
                 </div>
             </Container>
         </div>
