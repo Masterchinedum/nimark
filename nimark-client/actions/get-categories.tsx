d73155640a1a3010c81fc6a9c12a7411ba821227
CHINEDU KINGSLEY OKEREKE <Chinedu02k@gmail.com>
2024-07-27 08:59:26 +0000

    feat: Add lazy loading for category images in CategoryPage component

--- nimark-client/actions/get-categories.tsx
+++ nimark-client/actions/get-categories.tsx
@@ -9,3 +9,23 @@
 
 // export default getCategories;
 
+//client>actions>get-categories.tsx
+
+import { Category } from "@/types";
+
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/categories`;
+
+const getCategories = async (): Promise<Category[]> => {
+  try {
+    const res = await fetch(URL);
+    if (!res.ok) {
+      throw new Error(`Error: ${res.status}`);
+    }
+    return res.json();
+  } catch (error) {
+    console.error("Failed to fetch categories:", error);
+    return [];
+  }
+};
+
+export default getCategories;
