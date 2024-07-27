d92143fa7bc56639c8cf7eedde7e952b7de6729c
CHINEDU KINGSLEY OKEREKE <Chinedu02k@gmail.com>
2024-07-27 09:12:02 +0000

    feat: Add error handling to getCategory function

--- nimark-client/types.ts
+++ nimark-client/types.ts
@@ -2,13 +2,14 @@ export interface Billboard {
     id: string;
     label: string;
     imageUrl: string;
-}
+};
 
 export interface Category {
     id: string;
     name: string;
     billboard: Billboard;
-}
+};
+
 
 export interface Product {
     id: string;
@@ -19,20 +20,20 @@ export interface Product {
     size: Size;
     color: Color;
     images: Image[]
-}
+};
 
 export interface Image {
     id: string;
     url: string;
-}
+};
 
 export interface Size {
     id: string;
     name: string;
     value: string;
-}
+};
 export interface Color {
     id: string;
     name: string;
     value: string;
-}
+};
