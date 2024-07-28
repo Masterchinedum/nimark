285770215ea82c5166899bcb60aa2be003964a35
CHINEDU KINGSLEY OKEREKE <Chinedu02k@gmail.com>
2024-07-28 23:47:22 +0000

    refactor: Improve category page layout and filters

--- nimark-client/components/navbar.tsx
+++ nimark-client/components/navbar.tsx
@@ -9,7 +9,7 @@ export const revalidate = 0;
 const Navbar = async () => {
     const categories = await getCategories();
 
-    return (
+    return ( 
         <div className="border-b">
             <Container>
                 <div className="relative flex items-center h-16 px-4 sm:px-6 lg:px-8">
