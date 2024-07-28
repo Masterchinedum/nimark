7bb18386561962e467e901f9e6fe8db6e62b89bb
CHINEDU KINGSLEY OKEREKE <Chinedu02k@gmail.com>
2024-07-28 10:09:59 +0000

    refactor: Update get-categories API endpoint URL and remove unnecessary code
    This commit refactors the get-categories.tsx file by updating the API endpoint URL to use the NEXT_PUBLIC_API_URL environment variable. It also removes unnecessary commented code, improving code readability. These changes enhance the functionality and maintainability of the getCategories function.
    
    Co-authored-by: [Author Name]

--- nimark-client/actions/get-categories.tsx
+++ nimark-client/actions/get-categories.tsx
@@ -4,7 +4,7 @@ import { cache } from 'react';
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/categories`;
 
 export const getCategories = cache(async (): Promise<Category[]> => {
-  const res = await fetch(URL, { next: { revalidate: 3600 } }); // Revalidate every hour
+  const res = await fetch(URL);
   if (!res.ok) {
     throw new Error('Failed to fetch categories');
   }
