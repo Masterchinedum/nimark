042a91f5f52f3b5b4a4062b2f00babe770ae7acd
CHINEDU KINGSLEY OKEREKE <Chinedu02k@gmail.com>
2024-07-29 00:17:29 +0000

    refactor: Enable useCart hook in NavbarActions component
    This commit enables the useCart hook in the NavbarActions component of the nimark-client. The hook was previously commented out and is now uncommented, allowing for the functionality of managing the shopping cart in the application. This change enhances the user experience by providing seamless cart management.

--- nimark-client/components/navbar-actions.tsx
+++ nimark-client/components/navbar-actions.tsx
@@ -1,6 +1,6 @@
 "use client"
 import Button from '@/components/ui/button';
-// import useCart from '@/hooks/use-cart';
+import useCart from '@/hooks/use-cart';
 import { ShoppingBag } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
@@ -12,7 +12,7 @@ const NavbarActions = () => {
         setIsMounted(true);
     }, [])
 
-    // const cart = useCart();
+    const cart = useCart();
     const router = useRouter();
 
     if(!isMounted) {
@@ -28,7 +28,7 @@ const NavbarActions = () => {
                     color='white' 
                 />
                 <span className='ml-2 text-sm font-medium text-white'>
-                    {/* {cart?.items?.length} */}
+                    {cart?.items?.length}
                 </span>
             </Button>
         </div>
