5bf4cb006d9e67ca4be163da62d277607f6dfcfb
CHINEDU OKEREKE <Chinedu02k@gmail.com>
2024-08-20 15:00:48 +0000

    add brand.ts file

---
+++ nimark-admin/lib/utils/brand.ts
@@ -0,0 +1,20 @@
+import prismadb from "@/lib/prismadb";
+
+export async function getOrCreateDefaultBrand(storeId: string) {
+  let defaultBrand = await prismadb.brand.findFirst({
+    where: { storeId, isDefault: true }
+  });
+
+  if (!defaultBrand) {
+    defaultBrand = await prismadb.brand.create({
+      data: {
+        name: "Other",
+        imageUrl: "./cardboard-box.png", // Replace with an actual default image path
+        isDefault: true,
+        storeId,
+      }
+    });
+  }
+
+  return defaultBrand;
+}
